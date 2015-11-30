a61d1704e5708ee49c54bbc6c5043a24244b7e49
kylesnowschwartz <kyle.snowschwartz@gmail.com>
2015-11-30 16:15:32 +1300

    working

--- nslookup.rb
+++ nslookup.rb
@@ -6,20 +6,6 @@ require_relative './header.rb'
 require_relative './message.rb'
 require_relative './question.rb'
 
-def make_query(domain_name, dns_server)
-  header = Header.new(1, Header::QR_QUERY, Header::OP_CODE_QUERY, 0, 0, 0, Header::R_CODE_SUCCESS, 1, 0, 0, 0)
-  question = Question.new(domain_name, 1, Question::Q_CLASS_INTERNET)
-  message = Message.new(header, [question])
-
-  socket = UDPSocket.new
-  socket.connect(dns_server, 53)
-  socket.send(message.to_bytes, 0)
-
-  response = socket.recvfrom(1000)
-
-  Message.from_bytes(Unpacker.new(response[0]))
-end
-
 def recursive_query(domain_name, dns_server)
   message = make_query(domain_name, dns_server)
   puts "Trying ns #{dns_server}"
@@ -34,12 +20,27 @@ def recursive_query(domain_name, dns_server)
     unpack_record(record)
   end
 
-  # p message
   puts "ns #{dns_server} didn't have record, but returned ns servers #{ns_addresses}"
   ns_addresses.each do |ns_address|
     ip = recursive_query(domain_name, ns_address)
     return ip if ip
   end
+end
+
+private
+
+def make_query(domain_name, dns_server)
+  header = Header.new(1, Header::QR_QUERY, Header::OP_CODE_QUERY, 0, 0, 0, Header::R_CODE_SUCCESS, 1, 0, 0, 0)
+  question = Question.new(domain_name, 1, Question::Q_CLASS_INTERNET)
+  message = Message.new(header, [question])
+
+  socket = UDPSocket.new
+  socket.connect(dns_server, 53)
+  socket.send(message.to_bytes, 0)
+
+  response = socket.recvfrom(1000)
+
+  Message.from_bytes(Unpacker.new(response[0]))
 end
 
 def unpack_record(record)
